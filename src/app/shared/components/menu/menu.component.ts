3b98fce088fa7b0b43203f97ae8d0b98a1889cc9
Viviana Ramirez <ramirezvivi0102@gmail.com>
2023-11-22 18:37:14 -0500

    creacion modulo shared

---
+++ src/app/shared/components/menu/menu.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'shared-menu',
+  templateUrl: './menu.component.html',
+  styleUrls: []
+})
+export class MenuComponent {
+
+}
