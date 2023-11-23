0b6df09546b4163eeef7d65576a0b782cd6a78dc
Viviana Ramirez <ramirezvivi0102@gmail.com>
2023-11-23 17:15:17 -0500

    importamos en productsmodule el primengModule

--- src/app/prime-ng/prime-ng.module.ts
+++ src/app/prime-ng/prime-ng.module.ts
@@ -1,16 +1,22 @@
 import { NgModule } from '@angular/core';
-import { MenuModule } from 'primeng/menu';
+//import { MenuModule } from 'primeng/menu';
 import { MenubarModule } from 'primeng/menubar';
+import {ButtonModule } from 'primeng/button';
+import { CardModule } from 'primeng/card';
+import { FieldsetModule} from 'primeng/fieldSet';
+import { PanelModule } from 'primeng/panel';
 
 
 
 
 @NgModule({
-  
-  exports:[
-    MenuModule,
+  exports: [
+    // MenuModule,
     MenubarModule,
-  ]
-  
+    ButtonModule,
+    CardModule,
+    FieldsetModule,
+    PanelModule,
+  ],
 })
-export class PrimeNgModule { }
+export class PrimeNgModule {}
