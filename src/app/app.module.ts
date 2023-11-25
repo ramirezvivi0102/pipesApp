517226987509b3b72d904bcfd9669868bc672962
Viviana Ramirez <ramirezvivi0102@gmail.com>
2023-11-25 18:51:33 -0500

    se configura la fecha, hora global e idioma

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,22 @@
-import { NgModule } from '@angular/core';
+import { LOCALE_ID, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
 
+// configuracion del local de la app
+//COLOMBIA 
+import localesEsHN from '@angular/common/locales/es-CO';
+//FRANCES
+import localesfrBI from '@angular/common/locales/fr-BI';
 
+//declaramos la funcion registerLocaleData
+import { registerLocaleData } from '@angular/common';
+
+//realizamos el llamado de la funcion registerLocaleData
+registerLocaleData(localesEsHN);
+registerLocaleData(localesfrBI);
 
 
 @NgModule({
@@ -20,7 +31,10 @@ import { SharedModule } from './shared/shared.module';
     SharedModule,
 
   ],
-  providers: [],
+  //establecemos el idioma global por defecto ( español colombia )
+  providers: [
+    {provide: LOCALE_ID,useValue:'es-CO'}
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
